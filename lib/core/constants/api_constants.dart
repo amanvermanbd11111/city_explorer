7e95f1dc5eb8cdb3b0533f1add681ae6b84ccc3f
Aman Verma <amanvermanbd11111@gmail.com>
2025-09-02 13:13:44 +0530

    Update api_constants.dart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,7 +3,6 @@ class ApiConstants {
   static const String openWeatherMapBaseUrl = 'https://api.openweathermap.org/data/2.5';
   static const String websocketUrl = 'wss://ws.postman-echo.com/raw';
   
-  static const String openWeatherMapApiKey = '64e05a49a6607be31fc4eb29739addcf';
   
   static const int placesLimit = 10;
   static const int connectionTimeout = 30;
